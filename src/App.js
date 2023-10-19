10aba7df7cfee4f457e9af7555a96ed7641bd9cc
Tai Doan <taidoan@live.co.uk>
2023-10-19 11:53:38 +0100

    Testing TMDB api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOWQxYjY1MTk3MDJhMGM4YWEyMDljMDgyMGZkNzYwZCIsInN1YiI6IjY1MmZiNTQ2YTgwMjM2MDBmZDJkOWI0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.T4_lz5dW_04-vBkYjVXrA8w25MKLsPYqqiBj0t5CpLM",
+      },
+    };
+
+    const randomNumber = Math.floor(Math.random() * 500);
+
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${randomNumber}`,
+      options
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const results = data.results;
+        setList(results);
+      })
+      .catch((err) => console.error("Error:", err));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {list.map((movie) => (
+        <div key={movie.id}>
+          <h1>{movie.title}</h1>
+          <span>{movie.release_date}</span>
+          <p>{movie.overview}</p>
+          {/* Add other relevant data to display */}
+        </div>
+      ))}
     </div>
   );
 }
