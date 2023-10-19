89a25b8e3ca833aa4c77f667d3e751e22682a800
Tai Doan <taidoan@live.co.uk>
2023-10-19 13:01:06 +0100

    Testing form

--- src/App.js
+++ src/App.js
@@ -1,46 +1,8 @@
-import { useEffect, useState } from "react";
-import logo from "./logo.svg";
+import MovieSearch from "./Form";
 import "./App.css";
 
 function App() {
-  const [list, setList] = useState([]);
-
-  useEffect(() => {
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOWQxYjY1MTk3MDJhMGM4YWEyMDljMDgyMGZkNzYwZCIsInN1YiI6IjY1MmZiNTQ2YTgwMjM2MDBmZDJkOWI0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.T4_lz5dW_04-vBkYjVXrA8w25MKLsPYqqiBj0t5CpLM",
-      },
-    };
-
-    const randomNumber = Math.floor(Math.random() * 500);
-
-    fetch(
-      `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${randomNumber}`,
-      options
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const results = data.results;
-        setList(results);
-      })
-      .catch((err) => console.error("Error:", err));
-  }, []);
-
-  return (
-    <div className="App">
-      {list.map((movie) => (
-        <div key={movie.id}>
-          <h1>{movie.title}</h1>
-          <span>{movie.release_date}</span>
-          <p>{movie.overview}</p>
-          {/* Add other relevant data to display */}
-        </div>
-      ))}
-    </div>
-  );
+  return <MovieSearch />;
 }
 
 export default App;
